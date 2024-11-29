77f17a58fa74529e792bd162c84aa41b81fcfeac
Aaron Bridger <aaronjbridger@gmail.com>
2024-11-29 11:03:45 +0000

    updated header link

--- frontend/src/pages/Header/Header.jsx
+++ frontend/src/pages/Header/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/welcome">Home</Nav.Link>
             <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
           </Nav>
           <Nav className="ms-auto">
